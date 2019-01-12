b16ceced87183f1dea00a84f144471823575baba
Jen Lipton <jennifer.lipton@kroger.com>
2019-01-12 14:32:39 -0800

    Map with some marker

--- client/src/views/MapView.js
+++ client/src/views/MapView.js
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import ggLogo from '../assets/gg_logo.jpg';
 import GoogleMapReact from 'google-map-react';
 import MapVeggie from '../components/MapVeggie';
-import './MapView.css';
 
-//import { UpdateText } from './_redux/actions/test'
+import fancyMapStyles from './../assets/map-styles.json';
+import './MapView.css';
 
 class MapView extends Component {
     static defaultProps = {
@@ -15,15 +15,42 @@ class MapView extends Component {
         },
         zoom: 13
         };
+
   state = {
-  };
+    };
 
   componentDidMount() {
-  }
+    }
 
+    mapControlOptions() {
+        return {
+            'gestureHandling': 'greedy',
+            'mapTypeControl': false
+          }
+    }
 
   render() {
     const mapDescription = 'Map map map map';
+    const exampleProduce = { 
+        "produceType": "vegetable",
+        "variety":"orange",
+        "produceAmount": 12,
+        "produceName": "Pinapple",
+        "availability":{
+            "date": "12334566"
+        },
+        "pickUpLocation": {
+            "address": "2203 NE Oregon St",
+            "city": "Portland",
+            "state": "OR",
+            "zip":98765
+            },
+        "organic": true,
+        "seedSource": "Oregon",
+        "description": ""
+        }
+    const exampleResults = [ exampleProduce, exampleProduce ];
+
 
     return (
       <div id="map-view">
@@ -33,18 +60,25 @@ class MapView extends Component {
         </header>
         {/* // Important! Always set the container height explicitly */}
         <div id="map" style={{ height: '90vh', width: '100%' }}>
-        <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyDK6IbyqSKZFxGg6pcUPaprF8HDHRHcRVY' }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-        >
-            <MapVeggie
-            lat={45.5287205}
-            lng={-122.6432165}
-            // produce=thisProduce
-            // user= thisUser
-            />
-        </GoogleMapReact>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDK6IbyqSKZFxGg6pcUPaprF8HDHRHcRVY' }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                options={{ 
+                    styles: fancyMapStyles,
+                    ...this.mapControlOptions()
+                }}
+                >
+                {exampleResults.map( (result, i) => {
+                    return <MapVeggie
+                        key = {i}
+                        lat={45.5287205}
+                        lng={-122.6432165}
+                        produce={result}
+                        //user
+                    />
+                })}
+            </GoogleMapReact>
         </div>
     </div>
     );
